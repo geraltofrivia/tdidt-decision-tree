9067c41c2bbe4d5dbedc59f15b9a63c488deb1f9
Priyansh Trivedi <pc.priyansh@gmail.com>
2016-11-18 12:20:43 +0100

    Polished the shell script

--- run.sh
+++ run.sh
@@ -1,6 +1,9 @@
 echo  "
-This script will install graphviz, pickle. This assumes a unix system with python 2.7 and pip already installed. 
+This script will install graphviz,. This assumes a unix system with python 2.7 and pip already installed. 
+This script also automatically trains the TDIDT based on train data given in data/ and runs the test module as well.
 HINT: Hit Enter to Continue, or ctrl+C to quit this script.
+
+
 "
 read
 sudo pip install graphviz
